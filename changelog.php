20c479bf710a371a6370ee1d68ce25c88fa0c120
thisisstevi <steffen@wegers.eu>
2020-11-16 13:31:05 +0100

    Update changelog.php

--- changelog.php
+++ changelog.php
@@ -5,6 +5,7 @@ include 'inc/nav.php';
 
 <section>
 <h2>Changelog</h2><br><br>
+16.11.2020 - v.0.1.6 - Angebote erstellen um Gesamtrabatt erweitert
 12.11.2020 - v.0.1.5 - Angebote erstellen erstellt
 06.11.2020 - v.0.1.4 - Kundenauswahl f&uuml;r Angebot erstellen, Angebote anzeigen erstellt, Verzeichnisstruktur verbessert<br><br>
 05.11.2020 - v.0.1.3 - Suchfunktion in Kundenstamm integriert, CSS anpassungen<br><br>
