34879837be8b6232bec33dc095ef19331ce70c39
thisisstevi <steffen@wegers.eu>
2020-11-18 14:28:57 +0100

    Update angebot_erstellen.php

--- module/angebote/angebot_erstellen.php
+++ module/angebote/angebot_erstellen.php
@@ -3,6 +3,8 @@ include '../../inc/header.php';
 include '../../inc/nav.php';
 ?>
 
+     <link rel="stylesheet" href="../../css/angebot.css" type="text/css">
+
 <style>
 textarea { 
   width: 100%;
@@ -48,7 +50,7 @@ include '../../inc/connect.php';
 	$pos2ep = $_POST['ep'][1];
 	$pos2rab = $_POST['posrab'][1];
 
-    // 3. String für SQL-Anweisung erstellen
+    // 3. String fÃ¼r SQL-Anweisung erstellen
 	$insertString = "INSERT INTO angebote (kunde, anrede, datum, referenz, zahlungsbedingungen, netto, mwst, brutto, pos1anz, pos1einheit, pos1dsc, pos1ep, pos1rab, pos2anz, pos2einheit, pos2dsc, pos2ep, pos2rab)
 	VALUES ('$kunde', '$anrede', '$datum', '$referenz', '$zahlungsbedingungen', '$netto', '$mwst', '$brutto', '$pos1anz', '$pos1einheit', '$pos1dsc', '$pos1ep', '$pos1rab', '$pos2anz', '$pos2einheit', '$pos2dsc', '$pos2ep', '$pos2rab');";
 
@@ -67,11 +69,6 @@ include '../../inc/connect.php';
 
     // 2. Prüfe Radio-Button-Auswahl
 	if(isset($_GET["auswahl"])){
-
-    // 3. Datenbankabfrage starten
-    $id = $_GET["auswahl"];
-    $abfrage = "SELECT * FROM kunden WHERE id = $id";
-    $result = mysqli_query($connect, $abfrage);
 
     // 3. Datenbankabfrage starten
 	$id = $_GET["auswahl"];
@@ -95,6 +92,23 @@ include '../../inc/connect.php';
 	echo "<input name='id' type='hidden' value='$id'>";
 	echo "<textarea name='kunde' rows='10' cols='30'>$firma\n$vorname $nachname\n$strasse\n$plz $ort</textarea>";
 	echo "</div>";
+
+// Hier werden Systemeinstellungen ausgegeben
+
+    // 1. Systemeinstellungen Abfrage
+	$sysid = 1;
+	$sysabfrage = "SELECT mwst FROM einstellungen WHERE id = $sysid";
+	$sysresult = mysqli_query($connect, $sysabfrage);
+
+    // 2. Datensatz von Systemeinstellungen in Variablen speichern
+	$sysein = mysqli_fetch_assoc($sysresult);
+	$steuersatz = $sysein["mwst"];
+
+// Hier wird die Artikelsuche ausgegeben
+
+    // 1. Artikeldaten Abfrage
+	$abfrageartikelsuche = "select artikelname from artikel";
+	$resultartikelsuche = mysqli_query($connect, $abfrageartikelsuche) or die("Error " . mysqli_error($connect));
 ?>
 	
 	<div class="column3">
@@ -106,7 +120,7 @@ include '../../inc/connect.php';
 		<input type="text" name="referenz" placeholder="Referenz"/>
 	</div>
 
-<!-- Hier beginnt die Angebotsbearbeitung fŸr Positionen -->
+<!-- Hier beginnt die Angebotsbearbeitung fÂŸr Positionen -->
 <!-- Positionen -->
 <div id="docpos">
 	<table class="plist" style="font-size:10px;">
@@ -125,37 +139,36 @@ include '../../inc/connect.php';
 
 <br><br><br><br><br>
 
-<!-- Zahlungsbedingungen -->
-<div class="column3">
+  <div class="column70">
+  <div class="column50">
 	<textarea name="zahlungsbedingungen" id="zahlungsbedingungen" rows="10" cols="30" placeholder="Zahlungsbedingungen"></textarea>
-	<input class="button" type="submit" name="angebot-erstellen" value="Angebot erstellen">
-</div>
+  </div>  
 
-<div class="column3">
+  <div class="column50">
   <select name="gesamtrabatt" id="gesamtrabatt">
-    <option value="volvo">Rabatt</option>
-    <option value="saab">Nachlass</option>
+    <option value="Rabatt">Rabatt</option>
+    <option value="Nachlass">Nachlass</option>
   </select>
 
-<div id="rabatt"></div>
-<script> document.getElementById("rabatt").innerHTML = Rabatt(); </script>
+    <div id="rabatt"></div>
+    <script> document.getElementById("rabatt").innerHTML = Rabatt(); </script>
+  </div>
 
-</div>
-</div>
+	<input class="button" type="submit" name="angebot-erstellen" value="Angebot erstellen">
 
-<!-- Kalkulation -->
-<div class="column3">
+  </div>
+  <div class="column30">
 	<div style="width:100%;border: #4B9F93 1px solid;border-bottom:2px solid #F39200;"><br>
-		<label>&nbsp;Summe Netto:</label><input type="text" name="netto" id="netto" value="" readonly="" style="border:none;background-color:transparent;width:15%;text-align:right;" tabindex="32000"><br>
-		<label>&nbsp;Rabatt / Nachlass:</label><input type="text" name="gr" id="gr" value="" readonly="" style="border:none;background-color:transparent;width:15%;text-align:right;" tabindex="32000"><br>
-		<label>&nbsp;Summe Netto nach Abzug:</label><input type="text" name="nettoabzug" id="nettoabzug" value="" readonly="" style="border:none;background-color:transparent;width:15%;text-align:right;" tabindex="32000"><br>
-		<label>&nbsp;MwSt 19,00%:</label><input type="text" name="mwst" id="steuer" value="" readonly="" style="border:none;background-color:transparent;width:15%;text-align:right;" tabindex="32000"><br>
-		<label style="font-weight:bold;">&nbsp;Gesamt Brutto:</label><input type="text" name="brutto" id="brutto" value="" readonly="" style="border:none;background-color:transparent;width:15%;text-align:right;font-weight:bold;" tabindex="32000">
+		<label>&nbsp;Summe Netto:</label><input type="text" name="netto" id="netto" value="" readonly="" style="border:none;background-color:transparent;width:60%;text-align:left;" tabindex="32000"><br>
+		<label>&nbsp;Rabatt / Nachlass:</label><input type="text" name="gr" id="gr" value="" readonly="" style="border:none;background-color:transparent;width:60%;text-align:left;" tabindex="32000"><br>
+		<label>&nbsp;Summe Netto nach Abzug:</label><input type="text" name="nettoabzug" id="nettoabzug" value="" readonly="" style="border:none;background-color:transparent;width:60%;text-align:left;" tabindex="32000"><br>
+		<label>&nbsp;MwSt 19,00%:</label><input type="text" name="mwst" id="steuer" value="" readonly="" style="border:none;background-color:transparent;width:60%;text-align:left;" tabindex="32000"><br>
+		<label style="font-weight:bold;">&nbsp;Gesamt Brutto:</label><input type="text" name="brutto" id="brutto" value="" readonly="" style="border:none;background-color:transparent;width:60%;text-align:left;font-weight:bold;" tabindex="32000">
 	</div>
-</div>
-
+  </div>
 
 </form>
+&nbsp;
 
 </section>
 
