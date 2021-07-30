7cd94daf9be07f2907f4628b6f9002276b436c03
Tony Stark <steffen@wegers.eu>
2021-07-30 10:52:03 +0200

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host_name = 'localhost';
-$user_name = 'c3billy';
-$password = 'vssKNpZ_67';
-$database = 'c3billydb';
+$user_name = 'user';
+$password = 'password';
+$database = 'database';
 
 $connect = mysqli_connect($host_name, $user_name, $password, $database);
 mysqli_query($connect, "SET NAMES 'utf8'");
